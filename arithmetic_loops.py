3ad8e77fc2a9ccb1ef498a71d5a3149366c0145d
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 16:14:52 -0700

    Our Final version of Calc Exercise 2

---
+++ arithmetic_loops.py
@@ -0,0 +1,72 @@
+def add(nums):
+    result = 0
+    for i in nums:
+        result = result + i
+    return result
+
+def subtract(nums):
+    result = 0
+    for i in nums:
+        result = result - i
+    return result
+        
+def multiply(nums):
+    result = 1
+    for i in nums:
+        result = result * i
+    return result
+
+def divide(nums):
+    result = 1                  # / a     (((a/b)/c)/d)/e
+    for i in nums:
+        if i != 0
+    return num1 / num2 
+
+    first = nums[0] 
+    second = nums[1]
+    rest = nums[2:]      # nums[start:stop:step]   >=start  <stop +step   [2:6:2]  n[2] != n[2:]
+    result = first/second
+    for num in rest:
+        result = result / num
+
+    result = None
+    for num in nums:
+        if result is None:
+            result = num
+        else:
+            result = result / num
+
+    else 
+    for i + =1
+    return result
+
+def square(num1):
+    return num1**2
+    return result
+
+def cube(num1):
+    return num1**3
+    return result
+
+def power(num1, num2):
+    return num1**num2
+    return result
+
+def mod(num1, num2):
+    return num1 % num2
+    return result
+
+
+
+while (i++ < 10) {
+    
+}
+
+while True:
+    i += 1   # i = i + 1
+    if i == 10:
+        break
+
+if (nextval == get_from_user()) {
+    
+}
