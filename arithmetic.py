80ba8bf659bf911727344ac97a25cd0f2669a774
Hackbright Student <info@hackbrightacademy.com>
2014-10-01 16:13:59 -0700

    Our Final version of Calc Exercise 2

--- arithmetic.py
+++ arithmetic.py
@@ -1,22 +1,53 @@
-def add(num1, num2):
-    return num1 + num2
-def subtract(num1, num2):
-    return num1 - num2
+def add(nums):
+    result = 0
+    for i in nums:
+        result = result + i
+    return result
 
-def multiply(num1, num2):
-    return num1 * num2
+def subtract(*nums):
+    result = 0
+    for i in nums:
+        result = result - i
+    return result
+        
+def multiply(*nums):
+    result = 1
+    for i in nums:
+        result = result * i
+    return result
 
-def divide(num1, num2):
-    return num1 / num2
+def divide(*nums):
+    first = nums[0] 
+    second = nums[1]
+    rest = nums[2:]      
+    result = first/second
+    for num in rest:
+        if num != 0:
+            result = result / num
+    print result
 
-def square(num1):
-    return num1**2
+def square(*nums):
+    result = nums[0] ** 2
+    return result
+    print "The square of your first value is %d." % (result)   
+    return result
 
-def cube(num1):
-    return num1**3
+def cube(*nums):
+    result = nums[0] ** 3
+    return result
+    print "The cube of your first value is %d." % (result)   
+    return result
 
-def power(num1, num2):
-    return num1**num2
+def power(*nums):
+    result = nums[0]
+    for i in nums[1:]:
+        result = result ** i 
+    return result
 
-def mod(num1, num2):
-    return 10
+def mod(*nums):
+    result = nums[0]
+    for i in nums[1:]:
+        if i != 0:
+            result = result % i 
+    return result
+
